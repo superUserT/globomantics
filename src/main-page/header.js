9c65b4dbcc8b3ae214140b03913ba59627e90fbe
superUserT <rantshothabisomail@gmail.com>
2023-08-05 17:34:56 +0200

    header done

---
+++ src/main-page/header.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import logo from "./globelogo.png";
+
+
+
+// class Header extends Component {
+//     render() {
+//         return (
+//             <header className="row">
+//          <div className="col-md-5">
+//              <img src={logo} className="logo" alt="logo" />
+//          </div>
+//          <div className="col-md-7 mt-5 subtitle">
+//              {this.props.subtitle}
+//          </div>
+//      </header>
+//         );
+//     }
+// }
+
+const Header = ({ subtitle, title }) => (
+    <header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            {subtitle}
+        </div>
+    </header>
+);
+
+export default Header;
---
+++ src/main-page/index.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header 
+      subtitle="Providing Affordable housing in South Africa"
+      title="Globomantics" 
+      />
+    </div>
+  );
+}
+
+export default App;
